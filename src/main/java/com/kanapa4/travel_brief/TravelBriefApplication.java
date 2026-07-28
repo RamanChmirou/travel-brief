00f9a1fa27e4df9b3ae7d06aadb1708b35f31bb9
kanapa4 <chmirov2006@gmail.com>
2026-07-29 00:04:47 +0200

    Initial commit

---
+++ src/main/java/com/kanapa4/travel_brief/TravelBriefApplication.java
@@ -0,0 +1,13 @@
+package com.kanapa4.travel_brief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBriefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBriefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanapa4/travel_brief/TravelBriefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanapa4.travel_brief;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBriefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
